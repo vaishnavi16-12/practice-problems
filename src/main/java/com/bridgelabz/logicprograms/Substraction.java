bf103d2f7f26852c2c0e83bc9ac597682cee78fa
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-10-08 01:12:38 +0530

    Sub two numbers

---
+++ src/main/java/com/bridgelabz/logicprograms/Substraction.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.logicprograms;
+
+public class Substraction {
+    public static void main(String[] args) {
+        System.out.println("Substraction P and Q is ");
+
+    }
+}
