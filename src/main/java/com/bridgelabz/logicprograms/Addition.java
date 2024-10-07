757ce7dbf546af39b0982dc08816f46781cf6f76
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-10-07 17:32:10 +0530

    Addition of numbers

--- src/main/java/com/bridgelabz/logicprograms/Addition.java
+++ src/main/java/com/bridgelabz/logicprograms/Addition.java
@@ -3,5 +3,8 @@ package com.bridgelabz.logicprograms;
 public class Addition {
     public static void main(String[] args) {
         System.out.println("Hello");
+        int a=10, b=20;
+        int c= a+b;
+        System.out.println("Addition: " +c);
     }
 }
